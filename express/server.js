0bd039db8fcf550055d093ae6b9c9f83af0c52fb
Andrew Bennett <artosis@Andrews-MacBook-Pro.local>
2020-11-30 13:41:13 -0500

    Asdf

--- express/server.js
+++ express/server.js
@@ -47,7 +47,7 @@ router.get('/api', (req, res) => {
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
