20e9eac3be99f2f7acff75f78d4299eae40a4559
Andrew Bennett <artosis@Andrews-MacBook-Pro.local>
2020-11-30 13:57:40 -0500

    test

--- express/server.js
+++ express/server.js
@@ -5,7 +5,7 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-// const router = express.Router();
+const router = express.Router();
 // router.get('/', (req, res) => {
 //   res.writeHead(200, { 'Content-Type': 'text/html' });
 //   res.write('<h1>Hello from Express.js!</h1>');
@@ -25,7 +25,7 @@ const bodyParser = require('body-parser');
 
 
 // app.use(bodyParser.json());
-//app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 //app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 app.get('/api', (req, res) => {
   res.json({
