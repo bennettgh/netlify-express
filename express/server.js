9cde6223d2ccbaaf36aa9abdd41bbceaaf98dfc1
Andrew Bennett <artosis@Andrews-MacBook-Pro.local>
2020-11-30 13:47:03 -0500

    asdf

--- express/server.js
+++ express/server.js
@@ -6,18 +6,16 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
-const router = express.Router();
+app.use(bodyParser.json());
+//app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-router.get('/api', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({
     message: "Hello from the BeanAPI",
     data: {}
   })
 });
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
