077ac634fcc61c4142b36d9e56a812bcc0078e2d
Andrew Bennett <artosis@Andrews-MacBook-Pro.local>
2020-11-30 13:44:28 -0500

    reee

--- express/server.js
+++ express/server.js
@@ -7,36 +7,6 @@ const bodyParser = require('body-parser');
 
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<html>
-  <head>
-    <style>
-      * {
-        margin: 0;
-        padding: 0;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-weight: 300;;
-      }
-      .container {
-        padding: 40px;
-      }
-      h1 {
-        margin-bottom: 10px;;
-      }
-
-      body 
-    </style>
-  </head>
-  <body>
-    <div class='container'>
-      <h1>Welcome to the Bean API!</h1>
-      <h3>To access the API data, make a GET request to <a style="color: crimson;" href="https://fervent-mirzakhani-1e6826.netlify.app/api">/api</a></h3>
-    </div>
-  </body>
-</html>`);
-  res.end();
-});
 
 router.get('/api', (req, res) => {
   res.json({
@@ -47,7 +17,7 @@ router.get('/api', (req, res) => {
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
